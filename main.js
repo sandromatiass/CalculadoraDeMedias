12094f68f0737bc74d2027a4a6f45644ced96ba8
sandromatiass <devsandromatias83@gmail.com>
2024-07-16 23:09:43 -0300

    iniciando projeto 2 Ebac

---
+++ main.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('form-atividade');
+
+let linhas = '';
+let  imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando"/>';
+let  imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado"/>';
+
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+
+  const inputNomeAtividade = document.getElementById('nome-atividade');
+  const inputNotaAtividade = document.getElementById('nota-atividade');
+
+  let linha = '<tr>';
+  linha += `<td>${inputNomeAtividade.value}</td>`;
+  linha += `<td>${inputNotaAtividade.value}</td>`;
+  linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`;
+  linha +=  '</tr>';
+
+  linhas += linha;
+
+  const corpoTabela = document.querySelector('tbody');
+  corpoTabela.innerHTML = linhas;
+
+  inputNomeAtividade.value = '';
+  inputNotaAtividade.value = '';
+  
+});
